129f9874e9a0af2e0abbec73700d59b343175747
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-03-26 01:23:53 +0800

    Update workout_view.dart
    Remove Add Task Page and Link to TaskAddView.dart

--- lib/views/workout_view.dart
+++ lib/views/workout_view.dart
@@ -2,6 +2,7 @@
 /// Version 1.1.5
 
 import 'package:flutter/material.dart';
+import 'package:sweatpals/views/taskadd_view.dart';
 
 
 /// Workout Page 
@@ -10,14 +11,6 @@ class WorkoutView extends StatefulWidget {
 
   @override
   WorkoutViewState createState() => WorkoutViewState();
-}
-
-/// Add Task Pages 
-class TaskAddPage extends StatefulWidget {
-  const TaskAddPage({super.key});
-
-  @override
-  TaskAddPageState createState() => TaskAddPageState();
 }
 
 /// Workout Page Background 
@@ -96,7 +89,7 @@ class WorkoutViewState extends State<WorkoutView> {
                             final result = await Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => const TaskAddPage(),
+                                  builder: (context) => const TaskAddView(),
                                 ));
                             setState(() {
                               ListViewItemNumber += 1;
@@ -206,172 +199,3 @@ class WorkoutViewState extends State<WorkoutView> {
   }
 }
 
-/// Add Task Page Background 
-class TaskAddPageState extends State<TaskAddPage> {
-  /// TextBox Controoler initialise
-  final TextEditingController _textFieldControllerAddtask =
-      TextEditingController();
-  /// Task Item Name
-  String taskitemname = "";
-  /// Minutes Number
-  int intminselectedindex = -1;
-  /// Second Number
-  int intsecselectedindex = -1;
-  /// Check Add Button Status
-  bool boolTextinputed = false;
-
-  /// Throw Always When left the app
-  @override
-  void dispose() {
-    _textFieldControllerAddtask.dispose();
-    super.dispose();
-  }
-  /// Inital State
-  @override
-  void initState() {
-    super.initState();
-    // See Text change
-    _textFieldControllerAddtask.addListener(_onTextChanged);
-    }
-  /// Trim excesses Text
-  void _onTextChanged() {
-    final String text = _textFieldControllerAddtask.text;
-    if ((text.trim()) != "") {
-      setState(() {
-        boolTextinputed = true;
-      });
-    } else {
-      setState(() {
-        boolTextinputed = false;
-      });
-    }
-  }
-  /// Process for Add Task Page
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text("Add Task"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: SingleChildScrollView(
-          reverse: true,
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            //Label
-            const Text("Name Task :"),
-            //TextBox
-            TextField(
-              controller: _textFieldControllerAddtask,
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), hintText: null),
-            ),
-
-            const Text("    "),
-            // Label for timer
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Text("Minutes"),
-                  const Text("Second"),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // For Minute ListView
-                  Container(
-                    alignment: Alignment.center,
-                    height: 300,
-                    width: 80,
-                    child: ListView(
-                      children: [
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: 60,
-                            itemBuilder: (BuildContext context, int index) {
-                              return ListTile(
-                                title: Text(
-                                  '$index',
-                                  textAlign: TextAlign.center,
-                                ),
-                                tileColor: intminselectedindex == index
-                                    ? Colors.black12
-                                    : null,
-                                onTap: () {
-                                  setState(() {
-                                    intminselectedindex = index;
-                                  });
-                                },
-                              );
-                            })
-                      ],
-                    ),
-                  ),
-                  // For Second ListView
-                  Container(
-                    alignment: Alignment.center,
-                    height: 300,
-                    width: 80,
-                    child: ListView(
-                      children: [
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: 60,
-                            itemBuilder: (BuildContext context, int index) {
-                              return ListTile(
-                                title: Text(
-                                  '$index',
-                                  textAlign: TextAlign.center,
-                                ),
-                                tileColor: intsecselectedindex == index
-                                    ? Colors.black12
-                                    : null,
-                                onTap: () {
-                                  setState(() {
-                                    intsecselectedindex = index;
-                                  });
-                                },
-                              );
-                            })
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            const Text("    "),
-            //Add button
-            if (boolTextinputed &&
-                (intminselectedindex >= 0 || intsecselectedindex >= 0))
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    taskitemname = _textFieldControllerAddtask
-                        .text; // Save textbox input to a variable
-                    if (intsecselectedindex == -1) intsecselectedindex = 0;
-                    if (intminselectedindex == -1) intminselectedindex = 0;
-                    final data = {
-                      "taskname": taskitemname,
-                      "min": intminselectedindex,
-                      "sec": intsecselectedindex
-                    };
-                    Navigator.pop(context, data);
-                  });
-                },
-                child: const Text("Add"),
-              )
-          ]),
-        ),
-      ),
-    );
-  }
-}
