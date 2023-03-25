bc98a60f1fdbaea450892061cc5a5bf1b3727b32
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-03-26 01:41:17 +0800

    Update settings_view.dart
    Comment for Dartdoc

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -1,22 +1,28 @@
+/// Done by Chin poh, Jarrel , Cheng Feng , Hong Zhao , Ryan
+/// Version 1.1.5
+
 import 'package:flutter/material.dart';
 import 'package:sweatpals/constants/routes.dart';
 import 'package:sweatpals/services/auth/auth_service.dart';
 
+/// Setting Page
 class SettingsView extends StatefulWidget {
   const SettingsView({Key? key}) : super(key: key);
 
   @override
   SettingsViewState createState() => SettingsViewState();
 }
-
+/// Setting Page Background
 class SettingsViewState extends State<SettingsView> {
+  /// Username 
   String get username => AuthService.firebase().currentUser!.username!;
   // email might be null
+  /// Email Address
   String get email =>
       AuthService.firebase().currentUser!.email ?? 'guest (no email)';
 
   // todo: reset password and change email
-
+  /// Process for Setting Page
   @override
   Widget build(BuildContext context) {
     return Scaffold(
