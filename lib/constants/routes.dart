b76e37b163a26979a9d041262c08546040831b51
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-03-31 18:51:26 +0800

    Update routes.dart
    Comment for dartdoc

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,22 +1,39 @@
+/// Done by Chin poh, Jarrel , Cheng Feng , Hong Zhao , Ryan
+/// Version 1.1.5
+/// Routes Class
 class Routes {
+  /// Main Route path 
   static const mainRoute = '/main';
+  /// Login Route path
   static const loginRoute = '/login';
+  /// Registrater Route Path
   static const registerRoute = '/register';
+  /// Verify Email Route path
   static const verifyEmailRoute = '/verify-email';
-
+  /// Navigation bar Route path
   static const naviBarRoute = '/navi-bar';
+  /// Home Route path
   static const homeRoute = '/home';
+  /// Map Route path
   static const mapRoute = '/map';
+  /// Profile Route path
   static const profileRoute = '/profile';
+  /// Edit Profile Page Route path
   static const editProfileRoute = '/edit-profile';
+  /// Setting Route path
   static const settingsRoute = '/settings';
-
+  /// User Route path
   static const userRoute = '/user';
+  /// Gym Route path
   static const gymRoute = '/gym';
+  /// Friend Finder Route path
   static const friendFinderRoute = '/friend-finder';
+  /// Chat Box Route path
   static const chatboxRoute = '/chat-box';
-
+  /// Workout Route path
   static const workoutRoute = '/workout';
+  /// Route Tracker Route path
   static const routeTrackRoute = '/route-track';
+  /// Task Add Route path
   static const taskAddRoute = '/task-add';
 }
