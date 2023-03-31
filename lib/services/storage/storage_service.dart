4a14950b4218bb5c06efd1f1fe1617b2c74d13ff
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-03-31 17:11:41 +0800

    Update storage_service.dart
    Comment for dartdoc

--- lib/services/storage/storage_service.dart
+++ lib/services/storage/storage_service.dart
@@ -1,17 +1,21 @@
+/// Done by Chin poh, Jarrel , Cheng Feng , Hong Zhao , Ryan
+/// Version 1.1.5
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
+/// Storage Service Class
 class StorageService {
+  /// Initalise Firebase Storage Refrences
   final storageRef = FirebaseStorage.instance.ref();
-
+  /// Upload Image
   Future<String> uploadImage(String uid, File image) async {
     final uploadTask = storageRef.child("profileImg/$uid.jpg").putFile(image);
     final snapshot = await uploadTask;
     final url = await snapshot.ref.getDownloadURL();
     return url;
   }
-
+  /// Change Profile Picture
   Future<String> changeProfileImage(
     String uid,
   ) async {
@@ -28,7 +32,7 @@ class StorageService {
     }
     return "";
   }
-
+  /// Get Profile Picture
   Future<String> getProfileImage(String uid) async {
     final url = await storageRef.child("profileImg/$uid.jpg").getDownloadURL();
     return url;
