d200c2f17e41c843a38b7f4e6621fec08e4456dc
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-03-31 18:28:26 +0800

    Update auth_service.dart
    Comment for dartdoc

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,13 +1,19 @@
+/// Done by Chin poh, Jarrel , Cheng Feng , Hong Zhao , Ryan
+/// Version 1.1.5
 import 'package:sweatpals/services/auth/auth_provider.dart';
 import 'package:sweatpals/services/auth/auth_user.dart';
 import 'package:sweatpals/services/auth/firebase_auth_provider.dart';
 
+/// Authenticate Service Class
 class AuthService implements AuthProvider {
+  /// Initalise Authenticate Provider Class
   final AuthProvider provider;
-  const AuthService(this.provider);
 
+  /// Contrustor
+  const AuthService(this.provider);
   factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
 
+  /// Create User
   @override
   Future<AuthUser> createUser({
     required String username,
@@ -20,9 +26,11 @@ class AuthService implements AuthProvider {
         password: password,
       );
 
+  /// Retrieve Current User Authenticate Info
   @override
   AuthUser? get currentUser => provider.currentUser;
 
+  /// Login
   @override
   Future<AuthUser> logIn({
     required String email,
@@ -33,15 +41,17 @@ class AuthService implements AuthProvider {
         password: password,
       );
 
+  /// Logout
   @override
   Future<void> logOut() => provider.logOut();
 
+  /// Send Email Verification to User email
   @override
   Future<void> sendEmailVerification() => provider.sendEmailVerification();
-
+  /// Initialise Authenticator Provider
   @override
   Future<void> initialize() => provider.initialize();
-
+  /// Log in annonmyously
   @override
   Future<AuthUser> logInAnon({
     required String username,
@@ -49,12 +59,12 @@ class AuthService implements AuthProvider {
       provider.logInAnon(
         username: username,
       );
-
+  /// Update Display User Name 
   @override
   Future<void> updateDisplayName(String username) async {
     return await provider.updateDisplayName(username);
   }
-
+  /// Update User Photo URL 
   @override
   Future<void> updatePhotoURL(String photoURL) async {
     return await provider.updatePhotoURL(photoURL);
