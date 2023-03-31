426985c9e759b6e2616766b1e1e3e78836f05b0b
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-03-31 17:07:17 +0800

    Update toast.dart
    Comment for Dartdoc

--- lib/utilities/toast.dart
+++ lib/utilities/toast.dart
@@ -1,15 +1,26 @@
+/// Done by Chin poh, Jarrel , Cheng Feng , Hong Zhao , Ryan
+/// Version 1.1.5
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
-void showToast(
+/// Display Error Message 
+Future<void> showErrorDialog(
+  BuildContext context,
   String text,
 ) {
-  Fluttertoast.showToast(
-      msg: text,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.BOTTOM,
-      timeInSecForIosWeb: 1,
-      backgroundColor: Colors.grey,
-      textColor: Colors.white,
-      fontSize: 16.0);
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('An error occurred'),
+        content: Text(text),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
 }
