21796a29dbd3ea4cbfd67cbdf4cf44669879020a
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-03-31 18:27:48 +0800

    Update auth_user.dart
    Comment for dartdoc

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -1,14 +1,22 @@
+/// Done by Chin poh, Jarrel , Cheng Feng , Hong Zhao , Ryan
+/// Version 1.1.5
 import 'package:firebase_auth/firebase_auth.dart' show User;
 import 'package:flutter/foundation.dart';
 
+/// Authenticate User Class
 @immutable
 class AuthUser {
+  /// Text for UID
   final String uid;
+  /// Text for Username
   final String? username;
+  /// Text For Email Address
   final String? email;
+  /// Check Email Verify Status
   final bool isEmailVerified;
+  /// Text for Photo URL
   final String? photoURL;
-
+  /// Contrustor
   const AuthUser({
     required this.uid,
     required this.username,
@@ -16,7 +24,7 @@ class AuthUser {
     required this.isEmailVerified,
     required this.photoURL,
   });
-
+  /// Convert User object to AuthUser object
   factory AuthUser.fromFirebase(User user) => AuthUser(
         uid: user.uid,
         username: user.displayName,
