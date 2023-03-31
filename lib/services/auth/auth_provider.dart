7321a65d3c86240018a1d8dd4618f1be902f4dc2
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-03-31 18:29:08 +0800

    Update auth_provider.dart
    Comment for dartdoc

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -1,30 +1,32 @@
+/// Done by Chin poh, Jarrel , Cheng Feng , Hong Zhao , Ryan
+/// Version 1.1.5
 import 'package:sweatpals/services/auth/auth_user.dart';
-
+/// Abstract Authenticator Provider Class
 abstract class AuthProvider {
   Future<void> initialize();
-
+  /// Login
   Future<AuthUser> logIn({
     required String email,
     required String password,
   });
-
+  /// Retrieve Current User Authenticate Info
   AuthUser? get currentUser;
-
+  /// Create User
   Future<AuthUser> createUser({
     required String username,
     required String email,
     required String password,
   });
-
+  /// Logout
   Future<void> logOut();
-
+  /// Send Email Verification
   Future<void> sendEmailVerification();
-
+  /// Login Anonymous
   Future<AuthUser> logInAnon({
     required String username,
   });
-
+  /// Update Display Name
   Future<void> updateDisplayName(String username);
-
+  /// Update Photo URL
   Future<void> updatePhotoURL(String photoURL);
 }
