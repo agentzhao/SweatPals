d2771590775ced84b37ae5be1f485bf17452898c
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2023-04-04 21:07:42 +0800

    Update routetrack_view.dart
    test

--- lib/views/routetrack_view.dart
+++ lib/views/routetrack_view.dart
@@ -7,7 +7,6 @@ import 'dart:io';
 import 'package:geolocator/geolocator.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
